2044202e93dd8ee07596f29cab7b981d90c66c6f
Charlie Lindsay <charlieblindsay@gmail.com>
2023-03-25 11:52:07 +0000

    changed file names

--- data_processing.py
+++ data_processing.py
@@ -3,21 +3,24 @@ import os
 import pandas as pd
 from datetime import datetime
 
-# appdata_path = os.getenv('APPDATA')
-# path = appdata_path + '\Realterm\capture.txt'
+appdata_path = os.getenv('APPDATA')
+path = appdata_path + '\Realterm\capture.txt'
 
-path = 'capture.txt'
+# path = 'capture.txt'
 
 with open(path, 'r') as read_file:
-    temperature_data = [float(num.split('\n')[0]) for num in read_file.readlines()]
+    lines = read_file.readlines()
+    temperature_data = [float(line.split(' ')[0]) for line in lines]
+    PID_output = [float(line.split(' ')[1].split('\n')[0]) for line in lines]
 
 now = datetime.now()
 with open(f'{now.day}-{now.month} {now.hour}-{now.minute}-{now.second}.txt', 'w+') as write_file:
     write_file.writelines([f'{str(i)}\n' for i in temperature_data])
 
-time = [i * 0.3 for i in range(len(temperature_data))]
+time = [i * 0.1 for i in range(len(temperature_data))]
 
-plt.scatter(time, temperature_data)
+plt.plot(time, temperature_data)
 plt.xlabel('Time (s)')
 plt.ylabel('Temperature (deg C)')
+plt.savefig(f'{now.day}-{now.month} {now.hour}-{now.minute}-{now.second}')
 plt.show()
--- motor/motor.ino
+++ motor/motor.ino
@@ -6,7 +6,7 @@ const int stepsPerRevolution = 2038;
 
 // Creates an instance of stepper class
 // Pins entered in sequence IN1-IN3-IN2-IN4 for proper step sequence
-Stepper myStepper = Stepper(stepsPerRevolution, 8, 10, 9, 11);
+Stepper myStepper = Stepper(stepsPerRevolution, 12, 10, 13, 11);
 
 void setup() {
 	// Nothing to do (Stepper Library sets pins as outputs)
@@ -14,9 +14,9 @@ void setup() {
 
 void loop() {
 	// Rotate CW slowly at 10 RPM
-	myStepper.setSpeed(10);
+	myStepper.setSpeed(1);
 	myStepper.step(10); // TODO: test different number of steps to see which injects the most accurate volume of fluid
-  delay(2930);
+  delay(2665);
 	
 	// Rotate CCW quickly at 10 RPM
 	// myStepper.setSpeed(10);
